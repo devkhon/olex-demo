666338ab6a703b4e2b85aea65fb7db567ef84c21
zafar499 <zafzra1@example.com>
2025-02-26 09:57:52 +0500

    test.html tugadi

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('users/', include('users.urls')),
+    path('product/', include('product.urls')),
 ]
